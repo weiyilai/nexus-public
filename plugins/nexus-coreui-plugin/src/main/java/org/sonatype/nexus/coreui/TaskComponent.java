e68cbccd50ad713279510c0aca25fb5828df980e
AJ Waller <87146502+AJ-Waller@users.noreply.github.com>
2025-04-16 08:52:08 -0500

    Public export of Nexus code:   - 7fff56676f3c91c49bdcb5f0bf73f963cf059e65 by AJ Waller <87146502+AJ-Waller@users.noreply.github.com>
    GitOrigin-RevId: 7fff56676f3c91c49bdcb5f0bf73f963cf059e65

--- plugins/nexus-coreui-plugin/src/main/java/org/sonatype/nexus/coreui/TaskComponent.java
+++ plugins/nexus-coreui-plugin/src/main/java/org/sonatype/nexus/coreui/TaskComponent.java
@@ -485,7 +485,8 @@ public class TaskComponent
   }
 
   private static void appendPlanReconciliationText(StringBuilder lastRunResult, TaskState endState, TaskInfo taskInfo) {
-    if (OK.equals(endState) && taskInfo.getTypeId().equals(PLAN_RECONCILIATION_TASK_ID)) {
+    if (OK.equals(endState) && taskInfo.getTypeId().equals(PLAN_RECONCILIATION_TASK_ID) &&
+        taskInfo.getConfiguration().getBoolean("dryRun", false)) {
       lastRunResult.append(PLAN_RECONCILIATION_TASK_OK_TEXT);
     }
   }
