d0001603380e4dfa8b7cd2e08430de38ae49d391
Shreya Bera <shreya.bera@sonatype.com>
2025-12-01 16:23:30 +0530

    Public export of Nexus code:   - a3fa63ecdffa80ec183f2047170e719c6eddfb5b by Shreya Bera <shreya.bera@sonatype.com>
    GitOrigin-RevId: a3fa63ecdffa80ec183f2047170e719c6eddfb5b

--- plugins/nexus-repository-raw/src/main/java/org/sonatype/nexus/repository/raw/rest/RawGroupRepositoriesApiResource.java
+++ plugins/nexus-repository-raw/src/main/java/org/sonatype/nexus/repository/raw/rest/RawGroupRepositoriesApiResource.java
@@ -12,13 +12,18 @@
  */
 package org.sonatype.nexus.repository.raw.rest;
 
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+import org.sonatype.nexus.repository.raw.RawGroupApiRepository;
 import org.sonatype.nexus.repository.rest.api.AbstractGroupRepositoriesApiResource;
+import org.sonatype.nexus.repository.rest.api.FormatAndType;
+import org.sonatype.nexus.repository.rest.api.model.AbstractApiRepository;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -67,5 +72,16 @@ public abstract class RawGroupRepositoriesApiResource
       @ApiParam(value = "Name of the repository to update") @PathParam("repositoryName") final String repositoryName)
   {
     return super.updateRepository(request, repositoryName);
+  }
+
+  @GET
+  @Path("/{repositoryName}")
+  @Override
+  @ApiOperation(value = "Get repository", response = RawGroupApiRepository.class)
+  public AbstractApiRepository getRepository(
+      @ApiParam(hidden = true) @BeanParam final FormatAndType formatAndType,
+      @PathParam("repositoryName") final String repositoryName)
+  {
+    return super.getRepository(formatAndType, repositoryName);
   }
 }
